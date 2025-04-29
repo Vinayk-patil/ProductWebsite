b5539ed656530ae497d7d718b3509204bdbd9b37
vinayk patil <vinaykpatil24@gmail.com>
2025-04-29 23:02:33 +0000

    display only 4 product box per row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {filteredProducts.map((product) => (
           <Link
             href={`/products/${product.id}`}
